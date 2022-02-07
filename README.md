509f72f0ddd6768cb4b69c2ffd68b53282f120ca
Madhav Kauntia <mkauntia@gmail.com>
2022-02-08 02:53:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Steps to run
+1. Create a .env file
+2. Add the following properties
+```
+TWITTER_CONSUMER_KEY=
+TWITTER_CONSUMER_SECRET=
+TWITTER_ACCESS_TOKEN=
+TWITTER_ACCESS_TOKEN_SECRET=
+TWITTER_WEBHOOK_ENV=
+```
+To generate these keys, you have to apply for Twitter Developers account and then create a new application in the [Developer Portal Dashboard](https://developer.twitter.com/en/portal/dashboard).
+3. Run ```npm init``` and ```node index.js```
+
+## WIP
+1. Yet to add media_id to the reply tweet
+2. Extract tweetId from the tweet sent by a user (Twitter automatically shortens the URL)
---
+++ index.js
@@ -0,0 +1,58 @@
+const { Autohook } = require("twitter-autohook");
+const dotenv = require("dotenv");
+const client = require("twitter-api-client");
+dotenv.config();
+
+const twitterClient = new client.TwitterClient({
+  apiKey: process.env.TWITTER_CONSUMER_KEY,
+  apiSecret: process.env.TWITTER_CONSUMER_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+(async (start) => {
+  try {
+    const webhook = new Autohook();
+
+    webhook.on("event", async (event) => {
+      try {
+        if (event.direct_message_events) {
+          directMessages = event.direct_message_events;
+          const id = directMessages[0].id;
+          const message = await twitterClient.directMessages.eventsShow({
+            id: id,
+          });
+
+          const tweetId = message.event.message_create.message_data.text; // assuming that user will send tweet ID directly, might optimize for tweet url later on
+
+          twitterClient.tweets
+            .statusesUpdate({
+              status: "", // tweet content
+              in_reply_to_status_id: tweetId, // tweet id goes here
+              // add media here
+            })
+            .then(() => {})
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      } catch (err) {}
+    });
+
+    // Removes existing webhooks
+    await webhook.removeWebhooks();
+
+    // Starts a server and adds a new webhook
+    await webhook.start();
+
+    // Subscribes to your own user's activity
+    await webhook.subscribe({
+      oauth_token: process.env.TWITTER_ACCESS_TOKEN,
+      oauth_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    });
+  } catch (e) {
+    // Display the error and quit
+    console.error(e);
+    process.exit(1);
+  }
+})();
